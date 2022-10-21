2dbf0d9357ed08799168b3f08e79f661bb4e9eac
Wender Matheus <wender.technos@gmail.com>
2022-10-21 15:11:12 -0300

    commit de alteração de estado do tema escuro

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:initial_project/app_controller.dart';
 
 import 'home_page.dart';
 
 class AppWidget extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: HomePage(),
+    return AnimatedBuilder(
+      animation: AppController.instance,
+      builder: (context, child) {
+        return MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            brightness: AppController.instance.isDartTheme ? Brightness.dark : Brightness.light,
+            ),
+            home: HomePage(),
+        );
+      },
     );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:initial_project/app_controller.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,32 +11,29 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   int counter = 0;
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home-Page'),
       ),
-      
       body: Center(
-          child: GestureDetector(
-        child: Text(
-          'Contador: $counter', 
-          style: TextStyle(fontSize: 20),),
-        onTap: () {
-          setState(() {
-            counter++;
-          });
-        },
-      )),
-
+        child: Switch(
+            value: AppController.instance.isDartTheme,
+            onChanged: (value) {
+              AppController.instance.changeTheme();
+      }),
+    ),
+    
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
           setState(() {
             counter++;
           });
-        },),
+        },
+      ),
     );
   }
 }
